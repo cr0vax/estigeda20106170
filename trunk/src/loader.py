3cc16d62b0c6ab198f25de657b17aca13ca8b2cb
bruno.as.moreira@gmail.com <bruno.as.moreira@gmail.com@7e255dd8-c191-7b33-4b34-97fb70e32541>
2011-01-10 21:21:27 +0000

    Added test methods

--- trunk/src/loader.py
+++ trunk/src/loader.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 from connected_components import ConnectedComponents
+import time
 
 # LOADER
 def main():
     # global parameters
-    original_image_name = "ace.jpg"
+    original_image_name = ["cat-getImg.php.jpg",
+                           "ace.jpg",
+                           "alicate.jpg"]
 
     # identify image regions using connected components algorithm
-    cc = ConnectedComponents(original_image_name)
-    cc.identify_components()
+    lista = []
+
+    for a in range(len(original_image_name)):
+        print original_image_name[a]
+        
+        for i in range(10):
+            start = time.clock()
+            cc = ConnectedComponents(original_image_name[a])
+            cc.identify_components()
+            end = time.clock()
+
+        lista.append(end-start)
+        print 'Media: %r' % (sum(lista) / len(lista))
 
 if __name__ == "__main__":
     main()
